9e0106491b9eef3ca71cf0a2a1246a3eb7918732
k-taro <my.address.d@gmail.com>
2021-10-20 23:34:44 +0900

    fix design

--- src/components/pages/service/education/Education.tsx
+++ src/components/pages/service/education/Education.tsx
@@ -9,7 +9,7 @@ const Education = () => {
     <ul className="grid grid-cols-2 pc:gap-y-24 sp:gap-x-8 sp:gap-y-16">
       <li className="text-center text-black">
         <h3 className="pc:text-2xl sp:text-lg font-bold">フロントエンド</h3>
-        <div className="flex justify-center mt-5">
+        <div className=" mt-5">
           <FrontEndIcon />
         </div>
         <p className="pc:mt-6 sp:mt-4 sp:text-sm">
